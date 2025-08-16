ddf2a795ff853753d15d49c9a9b38b963a509e65
Sunita2405 <sunitasahu9033@gmail.com>
2025-08-17 01:24:11 +0530

    updated

---
+++ Javascript/class 2/script1.js
@@ -0,0 +1,139 @@
+// let a=20
+// let b=2
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// Assignment operator
+
+
+
+
+
+// comparision operator
+// let p=10
+// let q= 20
+// let r="10"
+// console.log(p==q);
+// console.log(p==r);
+// console.log(p!=q);
+
+//  console.log(p===q);  //(also check the datatype and value)
+//   console.log(p===r); 
+
+//    console.log(p!==q); 
+
+//logical operator
+
+// let x= true
+// let y=false
+// let z=true
+
+//multiplication(AND)
+
+//  console.log(x && y); 
+//   console.log(x && z);
+  
+  
+
+
+
+
+
+//  //unary operator
+
+//  a=10
+//  //increment
+//  console.log(++a);   //pre increment
+//    console.log(a++); //post increment
+//     console.log(a);
+
+
+//decrement
+
+// console.log(--a);
+// console.log(a--);
+    
+// !Data types-
+
+//1.Primitive types:(single variable store)
+//Number-
+// let num1 = 10
+// let num2=10.5
+// let num3 =-50.3
+// console.log(typeof num1);
+// console.log(typeof num2);
+// console.log(typeof num3);
+
+//String-
+
+// let str1 = "hello"
+// let str2 ="10"
+// let str3 = 'ftguhtdg878'
+// console.log(typeof str3);
+// console.log(str1[0]);
+
+// //Boolean
+
+// let bool1 = true
+// let bool2 = false
+// console.log(typeof bool1);
+
+// //undefined
+// let un
+// console.log(typeof un);
+// console.log(un)
+
+// //null
+// let n=null
+// console.log(n);
+// console.log(typeof n);
+
+// //BigInt
+// let big = BigInt(12345678934567)
+// console.log(typeof big);
+
+// //symbol
+// let symb= Symbol(122344444456)
+// console.log(typeof symb);
+
+//Non-primitive or reference type
+
+// //1.Array-heterogeneous
+
+// let marks =[90,80,70,40,35]
+// console.log(marks);
+// console.log(marks[1]);
+
+// //2.Object- (mutuable)
+//  let obj = {
+//     name:"abcd",
+//     age:23,
+//     marks: marks,
+//     address:
+//     {
+//         city:"bbsr",
+//         state:"odisha"
+//     }
+//  }
+//  console.log(obj);
+//   console.log(obj.name);
+//    console.log(obj["age"]);
+//     console.log(obj.address.city);
+//      console.log(obj.marks[1]);
+
+// //insert new key-
+// obj.cgpa=9.0
+//  console.log(obj);
+
+//  //delete a key
+//  delete obj.name
+//   console.log(obj);
+
+//   //*Function
+//   function example(){
+//     console.log("This is function");
+//   }
+//   example()
